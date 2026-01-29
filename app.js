f0caf05bd392350843925bcf24d4e63a428e5f6c
a38865 <a38865@mail.cmuh.org.tw>
2026-01-29 10:43:36 +0800

    fix: 修正 renderDetail 函數結尾的多餘空行

--- app.js
+++ app.js
@@ -1439,7 +1439,7 @@ async function renderDetail(nodeId, connectedNodeIds) {
         content.style.maxHeight = content.scrollHeight + 'px';
     });
 }
-}
+
 
 function buildResourceSummary(resource) {
     const rows = [];
